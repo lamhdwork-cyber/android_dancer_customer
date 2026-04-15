20ddffca08ab81ed0e78f747ef4c9fd74aae0b1e
Lâm Hoàng <lamhd.dev@gmail.com>
2026-04-16 00:12:09 +0700

    feat: add background for introduce

--- app/src/main/java/com/kantek/dancer/booking/presentation/screen/introduce/FetchIntroduceRepo.kt
+++ app/src/main/java/com/kantek/dancer/booking/presentation/screen/introduce/FetchIntroduceRepo.kt
@@ -11,19 +11,19 @@ class FetchIntroduceRepo(
     suspend operator fun invoke(): List<IIntroduce> {
         val rs = listOf(
             IntroduceDTO(
-                backgroundRes = 0,
+                backgroundRes = R.drawable.bg_introduce_1,
                 iconRes = R.drawable.ic_nav_search,
                 titleRes = R.string.introduce_title_2,
                 descriptionRes = R.string.introduce_description_2
             ),
             IntroduceDTO(
-                backgroundRes = 0,
+                backgroundRes = R.drawable.bg_introduce_2,
                 iconRes = R.drawable.ic_avatar_consultant,
                 titleRes = R.string.introduce_title_3,
                 descriptionRes = R.string.introduce_description_3
             ),
             IntroduceDTO(
-                backgroundRes = 0,
+                backgroundRes = R.drawable.bg_introduce_1,
                 iconRes = R.drawable.ic_nav_cases,
                 titleRes = R.string.introduce_title_1,
                 descriptionRes = R.string.introduce_description_1
